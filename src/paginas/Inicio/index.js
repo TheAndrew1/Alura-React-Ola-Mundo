c393bfaedfc309c2b4d293ff60ea8dcc3dcab3f3
Andre <andre.velasco@itaipuparquetec.org.br>
2025-03-11 13:17:08 -0300

    pagina inicial criada

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,10 +1,17 @@
 import Banner from "componentes/Banner";
+import styles from "./Inicio.module.css"
+import posts from "assets/json/posts.json"
+import Post from "componentes/Post";
 
 export default function Inicio() {
     return (
         <main>
             <Banner />
-            <h1>Olá Mundo!</h1>
+            <ul className={styles.posts}>
+                {posts.map((post) => (
+                    <Post key={post.id} post={post} />
+                ))}
+            </ul>
         </main>
     )
 }
